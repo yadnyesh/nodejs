ef6da1ca2883b15d8f6b46063c6047a7060c7361
yadnyesh <yadnyesh.juvekar@gmail.com>
2017-03-09 22:29:20 +0530

    Udemy Lecture 8

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ const os = require('os');
 var user = os.userInfo();
 
 //console.log(user);
-fs.appendFile('greetings.txt', 'Hello ' + user.username + '! ');
+fs.appendFile('greetings.txt', `Hello ${user.username}`);
