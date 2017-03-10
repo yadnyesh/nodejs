fec83d8251426422a78dba29d18fce1244cbbba9
yadnyesh <yadnyesh.juvekar@gmail.com>
2017-03-10 07:37:57 +0530

    Lecture 11

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ console.log('Starting App.js...');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
-const _ = require('loadash');
+const _ = require('lodash');
 
-console.log(_.isString(true));
-console.log(_.isString('Yadnyesh'));
+// console.log(_.isString(true));
+// console.log(_.isString('Yadnyesh'));
+var filteredArray = _.uniq([1,2,3,4,5,6,2,3,4,5,6]);
+console.log(filteredArray);
+
 
 // var user = os.userInfo();
 //
