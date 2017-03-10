bac6feab4b7411251c916d3db82c05f3f80a5668
yadnyesh <yadnyesh.juvekar@gmail.com>
2017-03-10 07:31:17 +0530

    Lecture 11

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ console.log('Starting App.js...');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
+const _ = require('loadash');
 
-var user = os.userInfo();
-
-//console.log(user);
-// fs.appendFile('greetings.txt', `Hello ${user.username} ! You are ${notes.age}. \n`);
-
-var res = notes.add(10,20);
-console.log(res);
+// var user = os.userInfo();
+//
+// //console.log(user);
+// // fs.appendFile('greetings.txt', `Hello ${user.username} ! You are ${notes.age}. \n`);
+//
+// var res = notes.add(10,20);
+// console.log(res);
