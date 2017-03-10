be97c55ea3c8fdb4ae2f9656e0370f7e8e552ba6
yadnyesh <yadnyesh.juvekar@gmail.com>
2017-03-10 06:58:29 +0530

    Lecture 5

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-console.log('Starting App...');
+console.log('Starting App.js...');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
 var user = os.userInfo();
 
