cdf856ca41b43a2859ed58947c5b0a10628fa580
yadnyesh <yadnyesh.juvekar@gmail.com>
2017-03-11 08:03:47 +0530

    March 11 2017

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ if (command == 'add') {
   console.log('Adding new note');
 } else if (command == 'list') {
   console.log('Listing all notes');
+} else if (command == 'read'){
+  console.log('Fetching note');
+} else if (command == 'remove') {
+  console.log('Removing note')
 } else {
   console.log('Command not recognized');
 }
