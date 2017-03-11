da4d9b60632fb918dc990d169de01bd85bf6e10c
yadnyesh <yadnyesh.juvekar@gmail.com>
2017-03-11 07:56:02 +0530

    March 11 2017

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 console.log('Starting App.js...');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
 const _ = require('lodash');
 
-var filteredArray = _.uniq([1,2,3,4,5,6,2,3,4,5,6]);
-console.log(filteredArray);
+const notes = require('./notes.js');
