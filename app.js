326d10c709a459284a86336bba781ac83d5ac7d5
yadnyesh <yadnyesh.juvekar@gmail.com>
2017-03-11 08:01:49 +0530

    March 11 2017

--- app.js
+++ app.js
@@ -9,4 +9,10 @@ var command = process.argv[2];
 
 console.log('Command: ', command);
 
-if
+if (command == 'add') {
+  console.log('Adding new note');
+} else if (command == 'list') {
+  console.log('Listing all notes');
+} else {
+  console.log('Command not recognized');
+}
