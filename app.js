16aeeb3743aebcf00da57b06192169e0861643f0
yadnyesh <yadnyesh.juvekar@gmail.com>
2017-03-12 08:10:08 +0530

    March 12 2017

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ if (command === 'add') {
   notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
   //console.log('Listing all notes');
-  notes.getAll
+  notes.getAll();
 } else if (command === 'read'){
   console.log('Fetching note');
 } else if (command === 'remove') {
--- notes.js
+++ notes.js
@@ -4,6 +4,10 @@ var addNote = (title, body) => {
   console.log('Adding Note', title, body);
 };
 
+var getAll = () => {
+  console.log('Getting all notes');
+}
+
 module.exports = {
   addNote
 };
