be4b790724f69e80dcb58eff2aa886d18515010e
yadnyesh <yadnyesh.juvekar@gmail.com>
2017-03-12 08:33:03 +0530

    March 12 2017

--- playground/json.js
+++ playground/json.js
@@ -1,8 +1,13 @@
-var obj = {
-  name: 'Yadnyesh'
-};
+// var obj = {
+//   name: 'Yadnyesh'
+// };
+//
+// var stringObj = JSON.stringify(obj);
+//
+// console.log(typeof stringObj);
+// console.log(stringObj);
 
-var stringObj = JSON.stringify(obj);
-
-console.log(typeof stringObj);
-console.log(stringObj);
+var personString = '{"name": "Yadnyesh", "age": 35}';
+var person = JSON.parse(personString);
+console.log(typeof person);
+console.log(person);
