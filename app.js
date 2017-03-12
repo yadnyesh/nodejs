2e57a4c34d2a0dd4501e35d92e10c03dc9841793
yadnyesh <yadnyesh.juvekar@gmail.com>
2017-03-12 08:20:43 +0530

    March 12 2017

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ if (command === 'add') {
 } else if (command === 'list') {
   notes.getAll();
 } else if (command === 'read'){
-  notes.read(argv.title);
+  notes.readNote(argv.title);
 } else if (command === 'remove') {
-  notes.read(argv.title);
+  notes.removeNote(argv.title);
 } else {
   console.log('Command not recognized');
 }
