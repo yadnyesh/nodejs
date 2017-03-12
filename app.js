fb26590d5daac7778d6214a80253738509294954
yadnyesh <yadnyesh.juvekar@gmail.com>
2017-03-12 08:01:14 +0530

    March 12 2017

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-
+const argv = yargs.argv;
 var command = process.argv[2];
 console.log('Command: ', command);
 console.log(process.argv);
