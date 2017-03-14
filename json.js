880083f29088223ad5d5e3b0191ecb5d152a20e9
yadnyesh <yadnyesh.juvekar@gmail.com>
2017-03-14 07:32:04 +0530

    Lecture 15

---
+++ json.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some Title',
+  body: 'Some body'
+}
+
+var originalNoteString = JSON.stringify(originalNote);
+
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+
+var note = JSON.parse(noteString);
